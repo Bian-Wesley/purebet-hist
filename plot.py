ed4985730e7fdef302bf32a822ca955f1ce75324
Bian-Wesley <134541601+Bian-Wesley@users.noreply.github.com>
2023-05-24 20:50:07 -0700

    Add files via upload

--- plot.py
+++ plot.py
@@ -6,5 +6,12 @@ print(df.head())
 #print the types
 print(df.dtypes)
 df["date"] = pd.to_datetime(df["date"])
+
+#set x axis as day, y axis as Dollars wagered
+#set plot title as Daily Volume at Purebet Since Launch
 plt.plot(df["date"], df["volume"])
+plt.suptitle("Daily Volume at Purebet Since Launch")
+plt.title("In $")
+plt.xlabel("Day")
+plt.ylabel("Dollars Wagered")
 plt.show()

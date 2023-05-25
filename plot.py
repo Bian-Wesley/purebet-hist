c959d5c73b33df3fa8c08bce18f5a393f20ebe88
Bian-Wesley <134541601+Bian-Wesley@users.noreply.github.com>
2023-05-24 21:03:48 -0700

    Add files via upload

--- plot.py
+++ plot.py
@@ -6,12 +6,24 @@ print(df.head())
 #print the types
 print(df.dtypes)
 df["date"] = pd.to_datetime(df["date"])
+df.set_index(df["date"])
+df = df.sort_index(ascending = False)
+print(df.head())
 
 #set x axis as day, y axis as Dollars wagered
 #set plot title as Daily Volume at Purebet Since Launch
 plt.plot(df["date"], df["volume"])
 plt.suptitle("Daily Volume at Purebet Since Launch")
 plt.title("In $")
-plt.xlabel("Day")
+plt.xlabel("Date")
+plt.ylabel("Dollars Wagered")
+plt.show()
+
+#cumulative volume
+df["cumulative_volume"] = df["volume"].cumsum()
+plt.plot(df["date"], df["cumulative_volume"])
+plt.suptitle("Cumulative Daily Volume at Purebet Since Launch")
+plt.title("In $")
+plt.xlabel("Date")
 plt.ylabel("Dollars Wagered")
 plt.show()
